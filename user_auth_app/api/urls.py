49d5be033d9cd261ccc0c2c598ed1d4ff7e3c0f0
Stephan <stephan-zager@web.de>
2025-05-06 20:42:22 +0200

    login view custom

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .view import RegistrationView, UserProfileList, UserProfileDetail
+from .view import RegistrationView, UserProfileList, UserProfileDetail,LoginView
 from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='register'),
-    path('login/', obtain_auth_token, name='login')
+    path('login/', LoginView.as_view(), name='login')
 ]
--- user_auth_app/api/view.py
+++ user_auth_app/api/view.py
@@ -1,15 +1,31 @@
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework import status
 from user_auth_app.models import UserProfile
 from .serializers import RegistrationSerializer, UserProfileSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'id': user.id, 
+            'username': user.username,
+            'email': user.email,
+        })
+
+
+
 class RegistrationView(APIView):
-    
-        
+           
     def post(self, request):
         serializer = RegistrationSerializer(data=request.data)
        
@@ -21,6 +37,7 @@ class RegistrationView(APIView):
                 'token': token.key,
                 'username': save_account.username,
                 'email': save_account.email,
+                'id': save_account.id,
             }
         else:
             data=serializer.errors
