8bd9c792371e033d65cf58c5e24f71794e7e052a
Stephan <stephan-zager@web.de>
2025-05-12 18:54:59 +0200

    permission Authenticated

--- task_app/api/view.py
+++ task_app/api/view.py
@@ -5,8 +5,10 @@ from .serializers import TasksSerializer
 from django.contrib.auth.models import User
 from rest_framework.exceptions import ValidationError
 from task_app.models import Tasks
+from user_auth_app.api.permissions import IsOwner
 
 
 class TasksView(generics.ListCreateAPIView):
     queryset = Tasks.objects.all()
     serializer_class = TasksSerializer
+    permission_classes = [permissions.IsAuthenticated,IsOwner]
