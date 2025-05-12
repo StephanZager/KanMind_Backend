47cd7385530f1c6e60e68aefd2b63a1509e41b45
Stephan <stephan-zager@web.de>
2025-05-12 18:16:25 +0200

    tasks model

--- task_app/models.py
+++ task_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from board_app.models import Board
 
-# Create your models here.
+
+class Tasks(models.Model):
+    
+    STATUS_CHOICES = [
+        ('to-do', 'To-Do'),
+        ('in-progress', 'In Progress'),
+        ('review', 'Review'),
+        ('done', 'Done'),
+    ]
+
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+    
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=100)
+    description = models.TextField
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES)
+    assignee = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='assigned_tasks')
+    reviewer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='reviewed_tasks')
+    due_date = models.DateField()
