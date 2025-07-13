e54af41cd0fcf8cad074342f5e9bd93edb185695
Stephan <stephan-zager@web.de>
2025-07-13 11:48:58 +0200

    boarder serializer details fixed request

--- kanban_app/api/serializers.py
+++ kanban_app/api/serializers.py
@@ -57,28 +57,13 @@ class BoardCreateSerializer(serializers.ModelSerializer):
 
 
 class BoardSerializerDetails(serializers.ModelSerializer):
-    members_details = MemberSerializer(
-        source='members', many=True, read_only=True
+    members = MemberSerializer(
+         many=True, read_only=True
     )
-
-    ticket_count = serializers.SerializerMethodField()
-    tasks_to_do_count = serializers.SerializerMethodField()
-    tasks_high_prio_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Board
-        fields = ['id', 'title', 'owner_id', 'members', 'members_details',
-                  'ticket_count', 'tasks_to_do_count', 'tasks_high_prio_count', 'tasks']
-
-    def get_ticket_count(self, obj):
-
-        return obj.tasks.count()
-
-    def get_tasks_to_do_count(self, obj):
-        return obj.tasks.filter(status='to-do').count()
-
-    def get_tasks_high_prio_count(self, obj):
-        return obj.tasks.filter(priority='high').count()
+        fields = ['id', 'title', 'owner_id', 'members', 'tasks']
 
 
 class TasksSerializer(serializers.ModelSerializer):
